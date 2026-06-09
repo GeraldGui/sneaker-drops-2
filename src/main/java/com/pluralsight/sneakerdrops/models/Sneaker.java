2dab8de90f06c924f50349572fb4387db867222d
Gerald G <geraldguignard2@gmail.com>
2026-06-09 10:36:17 -0400

    Adding rest of the files.

---
+++ src/main/java/com/pluralsight/sneakerdrops/models/Sneaker.java
@@ -0,0 +1,47 @@
+package com.pluralsight.sneakerdrops.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Sneaker {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String model;
+    private double price;
+    private int releaseYear;
+
+    public Sneaker() {
+    }
+
+    public Sneaker(String model, double price, int releaseYear) {
+        this.model = model;
+        this.price = price;
+        this.releaseYear = releaseYear;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+}
